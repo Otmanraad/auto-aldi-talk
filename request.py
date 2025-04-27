f4f1680fec725f69c22767dd0f8c918de3fb0bb2
Othman Raad Hussein <raad.h.othman@gmail.com>
2025-04-27 15:22:08 +0200

    Edit request.py, fixed some problems

--- request.py
+++ request.py
@@ -5,28 +5,19 @@ session = requests.Session()
 
 
 #################################
-# Manually set the cookies (Read the Readme on how to do it) 
-cookies = {
-    'AWSALB': 'your_aws_alb_cookie',
-    'AWSALBCORS': 'your_aws_albcors_cookie',
-    'rememberMe': 'true',
-    'session_id': 'your_session_id',
-    'user_id': 'your_user_id',
-    'is_logged': 'true',
-    # Add any other cookies you copied here
-}
-###################################
 
 
-session.cookies.update(cookies)
-
-# Headers (These are here to blend in)
+# Headers (These are here to blend in and for your login session for now)
 headers = {
     'Content-Type': 'application/json',
     'Accept': 'application/json, text/plain, */*',
     'Origin': 'https://www.alditalk-kundenportal.de',
     'Referer': 'https://www.alditalk-kundenportal.de/portal/auth/uebersicht/',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+	###################################
+#here you need to set the cookies that belong to your account go to alditalk-kundenportal.de/scs/bff/scs-209-selfcare-dashboard-bff/selfcare-dashboard/v1/offer/updateUnlimited and select the updateUnlimited file and copy the cookies in the developer menu
+	'Cookie': 'put your cookies here'
+	###################################
 }
 
 
@@ -48,14 +39,14 @@ payload_data = {
 
 
 # Limit for how many times the payload can be sent, if you go above aldi my detect this unusal behavior
-MAX_TIMES = 5
+MAX_TIMES = 9
 
 # Ask for number of times, with validation for the limit
 while True:
     num_times = int(input(f"How many request do you want to make (max {MAX_TIMES}): "))
-    if 1 <= num_times <= MAX_TIMES:
+    if 0 <= num_times <= MAX_TIMES:
         break
-    print(f"Please enter a number between 1 and {MAX_TIMES}.")
+    print(f"Please enter a number between 0 and {MAX_TIMES}.")
     
     
     
